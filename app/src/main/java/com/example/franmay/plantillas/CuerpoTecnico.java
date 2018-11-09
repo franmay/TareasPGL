939f1bc9fa7be56aa226be5c8d8ce4079d435453
franmay <fran_may@hotmail.es>
2018-11-09 23:36:16 +0000

    Segunda Entrega

--- app/src/main/java/com/example/franmay/plantillas/CuerpoTecnico.java
+++ app/src/main/java/com/example/franmay/plantillas/CuerpoTecnico.java
@@ -3,66 +3,65 @@ package com.example.franmay.plantillas;
 import android.os.Parcel;
 import android.os.Parcelable;
 
-public class Jugador implements Parcelable {
+public class CuerpoTecnico implements Parcelable {
 
     int foto;
     String nombre;
     String equipo;
     String pais;
-    int indiceEquipo;
-    String posicion;
-    int indicePosicion;
+    String cargo;
+    int indiceCargo;
     int edad;
 
 
-    public Jugador()
+    public CuerpoTecnico()
     {
         foto = 0;
         nombre = "";
         equipo = "";
-        pais="";
-        indiceEquipo = 0;
-        posicion = "";
-        indicePosicion = 0;
-        this.edad = 0;
+        pais = "";
+        cargo = "";
+        indiceCargo=0;
+        edad = 0;
     }
 
 
-    public Jugador(int foto, String nombre, String equipo, String pais, int indiceEquipo, String posicion,
-                   int indicePosicion, int edad)
+    public CuerpoTecnico(int foto, String nombre, String equipo, String pais, String cargo, int indiceCargo, int edad)
     {
         this.foto = foto;
         this.nombre = nombre;
         this.equipo = equipo;
         this.pais = pais;
-        this.indiceEquipo = indiceEquipo;
-        this.posicion = posicion;
-        this.indicePosicion = indicePosicion;
+        this.cargo = cargo;
+        this.indiceCargo = indiceCargo;
         this.edad = edad;
     }
 
-    protected Jugador(Parcel in) {
+
+    protected CuerpoTecnico(Parcel in)
+    {
         foto = in.readInt();
         nombre = in.readString();
         equipo = in.readString();
         pais = in.readString();
-        indiceEquipo = in.readInt();
-        posicion = in.readString();
-        indicePosicion = in.readInt();
+        cargo = in.readString();
+        indiceCargo = in.readInt();
         edad = in.readInt();
     }
 
-    public static final Creator<Jugador> CREATOR = new Creator<Jugador>() {
+
+    public static final Creator<CuerpoTecnico> CREATOR = new Creator<CuerpoTecnico>() {
         @Override
-        public Jugador createFromParcel(Parcel in) {
-            return new Jugador(in);
+        public CuerpoTecnico createFromParcel(Parcel in) {
+            return new CuerpoTecnico(in);
         }
 
         @Override
-        public Jugador[] newArray(int size) {
-            return new Jugador[size];
+        public CuerpoTecnico[] newArray(int size) {
+            return new CuerpoTecnico[size];
         }
     };
+
 
     public int getFoto() {
         return foto;
@@ -80,16 +79,12 @@ public class Jugador implements Parcelable {
         return pais;
     }
 
-    public int getIndiceEquipo() {
-        return indiceEquipo;
+    public String getCargo() {
+        return cargo;
     }
 
-    public String getPosicion() {
-        return posicion;
-    }
-
-    public int getIndicePosicion() {
-        return indicePosicion;
+    public int getIndiceCargo() {
+        return indiceCargo;
     }
 
     public int getEdad() {
@@ -113,16 +108,12 @@ public class Jugador implements Parcelable {
         this.pais = pais;
     }
 
-    public void setIndiceEquipo(int indiceEquipo) {
-        this.indiceEquipo = indiceEquipo;
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
     }
 
-    public void setPosicion(String posicion) {
-        this.posicion = posicion;
-    }
-
-    public void setIndicePosicion(int indicePosicion) {
-        this.indicePosicion = indicePosicion;
+    public void setIndiceCargo(int indiceCargo) {
+        this.indiceCargo = indiceCargo;
     }
 
     public void setEdad(int edad) {
@@ -135,15 +126,16 @@ public class Jugador implements Parcelable {
         return 0;
     }
 
+
     @Override
-    public void writeToParcel(Parcel dest, int flags) {
+    public void writeToParcel(Parcel dest, int flags)
+    {
         dest.writeInt(foto);
         dest.writeString(nombre);
         dest.writeString(equipo);
         dest.writeString(pais);
-        dest.writeInt(indiceEquipo);
-        dest.writeString(posicion);
-        dest.writeInt(indicePosicion);
+        dest.writeString(cargo);
+        dest.writeInt(indiceCargo);
         dest.writeInt(edad);
     }
 }
